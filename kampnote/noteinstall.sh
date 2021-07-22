7447183729cd49444776bb23009e8126a8caef0a
kimkeehwan <wempi21@gmail.com>
2021-07-22 17:22:57 +0900

    passwd line removed

--- kampnote/noteinstall.sh
+++ kampnote/noteinstall.sh
@@ -201,9 +201,6 @@ echo "downloaded kampnote service file"
 
 sudo cp $local_tmp_dir/kampnote.service /etc/systemd/system/kampnote.service
 
-echo "new password kampuser account"
-sudo passwd kampuser
-
 echo "starting kampnote service..."
 sudo systemctl daemon-reload
 sudo systemctl enable kampnote.service
